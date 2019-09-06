c290cf0d26fbe376530447015757953cf0038dff
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-07 00:03:05 +0530

    n

---
+++ 2-getting_inputs.c
@@ -0,0 +1,18 @@
+/*C progam to print Hello World
+  Author: abhijeet
+  Created on 6 Sept, 2019, 09:05 PM
+*/
+#include<stdio.h>
+int main()
+{
+  int n,choice;   //integer data type declaration
+  float m;        //float data type declaration
+  double d;       //double data type declaration
+  char c;         //character data type declaration
+  //Menu for choosing type of data to store and display
+  printf("Enter the choice of data type you wish to enter\n");
+  scanf("%d",&choice);
+  scanf("%d",&n);
+  printf("Input value is %d \n",n);
+  return 0;
+}
