084641b8966e1c26afe1a07338619a43728c3c8f
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-09 18:07:08 +0530

    c

---
+++ 11-square-onscreen.c
@@ -0,0 +1,17 @@
+/*C progam to print a square using escape squences
+  Author: abhijeet
+  Created on 9 Sept, 2019, 06:06 PM
+*/
+#include <stdio.h>
+ main()
+{
+  printf("There's a square: \n\n");
+  printf("     ..................                \n");
+  printf("    |                  |               \n");
+  printf("    |                  |               \n");
+  printf("    |                  |               \n");
+  printf("    |                  |               \n");
+  printf("    |                  |               \n");
+  printf("    |                  |               \n");
+  printf("     ''''''''''''''''''                \n");
+}
