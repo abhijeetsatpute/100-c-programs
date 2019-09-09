934ef32bbd8836b0f4d7b4b457735fe696529402
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-10 04:43:24 +0530

    c

---
+++ 14-n_grtr_than_10.c
@@ -0,0 +1,24 @@
+/*C progam to Check the number is greater than or less than 10
+  Author: abhijeet
+  Created on 10 Sept, 2019, 04:41 AM
+*/
+#include <stdio.h>
+ main()
+{
+  int a;
+  printf("Enter The number to be Checked\nA: ");
+  scanf("%d",&a );
+  if(a<10)
+    {
+      printf("%d is Smaller than 10",a);
+    }
+  else if (a>10)
+    {
+      printf("%d is Greater than 10",a);
+    }
+  else
+    {
+      printf("Number is equal to 10");
+    }
+  getch();
+}
