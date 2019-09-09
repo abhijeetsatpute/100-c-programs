ca49ac13ba580eb1d1e214c9be9c1b539dac33b1
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-09 17:52:13 +0530

    c

---
+++ 9-escape-sequences-in-c.c
@@ -0,0 +1,26 @@
+/*C progam for escape sequences in printf
+  Author: abhijeet
+  Created on 6 Sept, 2019, 05:50 PM
+
+\a    Alarm or Beep
+\b    Backspace
+\f    Form Feed
+\n    New Line
+\r    Carriage Return
+\t    Tab (Horizontal)
+\v    Vertical Tab
+\\    Backslash
+\'    Single Quote
+\"    Double Quote
+\?    Question Mark
+\ooo  octal number
+\xhh  hexadecimal number
+\0    Null
+
+*/
+#include <stdio.h>
+int main()
+{
+  printf("https:\\\\www.google.com\\images\\image.jpg");
+  return 0;
+}
