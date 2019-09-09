f47a065fb517e0385df4f55fcae8e6dae2181f56
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-09 16:37:28 +0530

    c

--- 4-area_triangle.c
+++ 4-area_triangle.c
@@ -1,12 +1,16 @@
+/*C progam to print arae of triangle
+  Author: abhijeet
+  Created on 9 Sept, 2019, 04:36 PM
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-main()
+int main()
 {
-	float b,h,area;
-	printf("Enter the base and height of the triangle :");
-	scanf("%f %f",&b,&h);
-	area=(1/2)*b*h;
-	printf("%f",area);
-	getch();
+    float area,h,b;
+		printf("Enter the Height and Base\n");
+    scanf("%f%f",&h,&b);
+    area=0.5*b*h;
+    printf("%6.2f",area);
+    return 0;
 }
