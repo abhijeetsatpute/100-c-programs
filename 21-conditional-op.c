b9226c7063a7dff9de5cc2fa1a8a9cdf2fe4cbac
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-10 06:16:02 +0530

    c

--- 21-conditional-op.c
+++ 21-conditional-op.c
@@ -1,7 +1,7 @@
 /*C progam for conditional operators
                                       (condition) ? if_true_do : else_this ;
   Author: abhijeet
-  Created on 9 Sept, 2019, 06:11 AM
+  Created on 9 Sept, 2019, 06:14 AM
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,5 +11,7 @@ main()
   printf("Enter two values :");
   scanf("%d%d",&a,&b);
   (a>b)?printf("%d is greater",a):printf("%d is greater",b);
+  //Assign values directly using conditional operators
+  printf("\n%d is greater",(a>b)?a:b);
   getch();
 }
