1a14bd2bdcb01a35f5d26997050eb2595306bd98
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-12 20:20:39 +0530

    c

---
+++ 53-String.c
@@ -0,0 +1,17 @@
+/*C progam -Array of char or String
+
+Author: abhijeet
+  Created on 12 Sept, 2019, 10:34 AM
+*/
+#include<stdio.h>
+int main()
+{
+    int i;
+    char x[10]={'a','b','h','i','j','e','e','t','\0'};
+    for(i=0;x[i]!='\0';i++)
+        printf("%c",x[i]);     //chars till null
+    printf("\n%s\n",x );       //whole string
+    puts(x);                   //withour format specifier cursor to next line
+    getch();
+    return 0;
+  }
