9b7d37b96a4530fab96ac6896566609288ab2ff1
Abhijeet Satpute <abhi111sat@gmail.com>
2019-09-16 11:18:07 +0530

    c

---
+++ 68-FILE.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main()
+{
+  int i;
+  FILE *fp;
+  char s[]="Hello students";
+  fp=fopen("f1.txt","w");
+  if(fp==NULL){
+    printf("File not found");
+    exit(1);
+  }
+  for(i=0;i<strlen(s);i++)
+    fputc(s[i],fp);
+  fclose(fp);
+  getch();
+}
